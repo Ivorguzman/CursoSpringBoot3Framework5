1f2c23c6fe22a498436b16887169ec630e1bdd3c
ivorguzman <ivorguzman7@gmail.com>
2025-02-11 22:50:08 -0400

    Respaldo 11022025

--- jpa-demo/src/main/java/net/tinajero/model/Categoria.java
+++ jpa-demo/src/main/java/net/tinajero/model/Categoria.java
@@ -69,5 +69,53 @@ public class Categoria {
 		return "Categoria [id=" + this.id + ", nombre=" + this.nombre + ", descripcion=" + this.descripcion + "]";
 	}
 
+	/*
+	 * Lo que hace Spring detrás de Bambalinas:
+	 * Mapeo ORM:(
+	 * 
+	 * En términos sencillos:
+	 * Ubicar: Mapeo implica ubicar las clases y atributos de Java en las correspondientes tablas y columnas de la base de
+	 * datos. Es decir, identificar dónde encaja cada cosa.
+	 * 
+	 * Poblar: Una vez ubicados, poblar significa llenar esas tablas y columnas con los datos de los objetos Java, y
+	 * viceversa.
+	 * 
+	 * Así que, al mapear, efectivamente estás ubicando y poblando datos entre tu aplicación Java y tu base de datos.)
+	 * 
+	 * Entidad a Tabla: Cuando la aplicación se inicia, Spring Data JPA escanea las clases anotadas con @Entity y mapea la
+	 * clase Categoria a la tabla "categorias" en la base de datos.
+	 * 
+	 * Campos a Columnas: Cada campo de la clase Categoria se mapea a una columna en la tabla "categorias".
+	 * 
+	 * Generación de SQL:
+	 * Inserción: Cuando se guarda una nueva instancia de Categoria, Spring Data JPA genera una consulta INSERT para agregar
+	 * una nueva fila a la tabla "categorias".
+	 * 
+	 * sql
+	 * INSERT INTO categorias (nombre, descripcion) VALUES (?, ?);
+	 * Actualización: Cuando se actualiza una instancia de Categoria, se genera una consulta UPDATE para modificar una fila
+	 * existente en la tabla "categorias".
+	 * 
+	 * sql
+	 * UPDATE categorias SET nombre = ?, descripcion = ? WHERE id = ?;
+	 * Eliminación: Cuando se elimina una instancia de Categoria, se genera una consulta DELETE para eliminar una fila de la
+	 * tabla "categorias".
+	 * 
+	 * sql
+	 * DELETE FROM categorias WHERE id = ?;
+	 * Consulta: Cuando se buscan instancias de Categoria, se generan consultas SELECT para recuperar datos de la tabla
+	 * "categorias".
+	 * 
+	 * sql
+	 * SELECT * FROM categorias;
+	 * 
+	 * Transacciones:
+	 * Las operaciones de persistencia (guardar, actualizar, eliminar) se ejecutan dentro de transacciones para asegurar que
+	 * los cambios en la base de datos sean consistentes.
+	 * 
+	 * Gestión del Ciclo de Vida:
+	 * Spring Data JPA gestiona el ciclo de vida de las entidades, asegurando que se sincronizan correctamente con la base
+	 * de datos cuando se realizan cambios.
+	 */
 
 }
