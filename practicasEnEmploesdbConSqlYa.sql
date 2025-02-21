70adfe80ae93c2ecd45c06b1a865a788aa6d426f
ivorguzman <ivorguzman7@gmail.com>
2025-02-21 01:53:31 -0400

    Respaldo 21022025

---
+++ practicasEnEmploesdbConSqlYa.sql
@@ -0,0 +1,15 @@
+DESCRIBE categorias;
+DESCRIBE perfiles;
+DESCRIBE solicitudes;
+DESCRIBE usuarioperfil;
+DESCRIBE usuarios;
+DESCRIBE vacantes;
+DESCRIBE empleosdb.perfiles;
+SELECT  * FROM empleosdb.categorias;
+SELECT empleosdb.categorias.descripcion, empleosdb.categorias.nombre, empleosdb.categorias.id FROM empleosdb.categorias;
+SELECT * FROM empleosdb.categorias;
+SELECT * FROM empleosdb.solicitudes;
+SELECT * FROM empleosdb.usuarioperfil;
+SELECT * FROM empleosdb.usuarios;
+SELECT * FROM empleosdb.vacantes;
+
